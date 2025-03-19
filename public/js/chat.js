53c1f9b28b6b229cd0a393efbd8b2ab03833ac74
AdrainRazini <160901383+AdrainRazini@users.noreply.github.com>
2025-03-19 14:28:06 -0300

    chat ggg4

---
+++ public/js/chat.js
@@ -0,0 +1,34 @@
+// public/chat.js
+const socket = io();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const username = localStorage.getItem('username');
+  if (!username) {
+    window.location.href = 'index.html';
+  } else {
+    socket.emit('login', username);
+  }
+});
+
+socket.on('userConnected', (username) => {
+  document.getElementById('chat-box').innerHTML += `<p><strong>${username} entrou no chat.</strong></p>`;
+});
+
+socket.on('message', (msg) => {
+  const chatBox = document.getElementById('chat-box');
+  chatBox.innerHTML += `<p><strong>${msg.user}:</strong> ${msg.text}</p>`;
+  chatBox.scrollTop = chatBox.scrollHeight;
+});
+
+socket.on('userDisconnected', (username) => {
+  document.getElementById('chat-box').innerHTML += `<p><em>${username} saiu do chat.</em></p>`;
+});
+
+function sendMessage() {
+  const messageInput = document.getElementById('message');
+  const message = messageInput.value;
+  if (message) {
+    socket.emit('chatMessage', message);
+    messageInput.value = '';
+  }
+}
